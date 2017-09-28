586871db6ac8454191c8132d8736da28b7816cd9
leekingu <leekingu1990@gmail.com>
2017-09-28 10:43:25 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,40 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	oHd.style.width = document.documentElement.clientWidth + "px";
 	oHd.style.height = document.documentElement.clientHeight + "px";
 	oHd.style.backgroundSize = document.documentElement.clientWidth + "px auto";
-	window.onresize = window.onscroll = function() {
-		var oHd = document.querySelector("header");
-		var oSimShow = document.querySelector(".sim-show");
-		oHd.style.width = document.documentElement.clientWidth + "px";
-		oHd.style.height = document.documentElement.clientHeight + "px";
-		oHd.style.backgroundSize = document.documentElement.clientWidth + "px auto";
-		oOther.style.width = document.documentElement.clientWidth + "px";
-		oOther.style.height = document.documentElement.clientHeight + "px";
-		oOther.style.backgroundSize = document.documentElement.clientWidth + "px auto";
-		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		var scale = oMe.offsetTop - scrollTop;
-		oMe.style.backgroundPosition = "center " + (scale / 3 - 200) + "px";
-		oSha.style.opacity = 0.8 * (1 - scale / 200)
-	};
-	(function() {
-		var oUl = oMe.getElementsByTagName("ul")[0];
-		var oOl = oMe.getElementsByTagName("ol")[0];
-		var aLi = oUl.children;
-		var aBtn = oOl.children;
-		for (var i = 0; i < aBtn.length; i++) {
-			(function(index) {
-				aBtn[i].onmouseover = function() {
-					for (var i = 0; i < aBtn.length; i++) {
-						aBtn[i].className = "";
-						aLi[i].style.opacity = 0
-					}
-					aBtn[index].className = "on";
-					aLi[index].style.opacity = 1
-				}
-			})(i)
-		}
-	})(function(){
-		eval();
-	}());
 	(function() {
 		var oBtn = oHd.querySelector(".me-btn");
 		oBtn.onmouseover = function() {}
